23b8b5e6fe174b7b9f047dbee97b7d4b350f9b06
OIKKac <roenw92jpw@gmail.com>
2020-02-11 09:04:46 +0900

    Initial commit

---
+++ src/main/java/com/moontech/sms/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.moontech.sms.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+  @Controller
+  public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String test(){
+      return "index";
+    }
+  }
